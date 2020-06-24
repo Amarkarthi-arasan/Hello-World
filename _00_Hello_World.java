c616e56f2230e614ca642e5756b47fbe4db8fec6
Amar Karthi ARASAN <62425477+Amarkarthi-arasan@users.noreply.github.com>
2020-06-24 11:10:54 +0530

    Add files via upload
    after adding comments

--- _00_Hello_World.java
+++ _00_Hello_World.java
@@ -3,9 +3,11 @@ public class _00_Hello_World
 
 	public static void main(String[] args)
 	{
+		//system.out.println > prints and moves cursor to new line
 		System.out.println("My first Java Program");
 		System.out.println("This will be printed in a new line");
 		
+		//system.out.print > prints and keeps the cursor at the same point
 		System.out.print("India");
 		System.out.print("is");
 		System.out.print("my");
